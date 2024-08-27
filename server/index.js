9bf794907495601f37d478d6b94d9128a1c71cae
Ashyboban <ashyboban14@gmail.com>
2024-08-27 10:33:19 +0530

    connected to database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://ashy:ashy@cluster0.7xnsr.mongodb.net/mini_project?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+  console.log('connected to mongodb');
+}).catch((err)=>{
+  console.log(err);
+});
 const app = express();
 
 app.get('/', (req, res) => {
