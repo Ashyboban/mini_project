271f263a12ae4b8cb713f60ebbec200fa5f6c08f
Ashyboban <ashyboban14@gmail.com>
2024-08-28 10:13:20 +0530

    create test api route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
+
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 mongoose.connect("mongodb+srv://ashy:ashy@cluster0.7xnsr.mongodb.net/mini_project?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
   console.log('connected to mongodb');
 }).catch((err)=>{
@@ -7,10 +9,8 @@ mongoose.connect("mongodb+srv://ashy:ashy@cluster0.7xnsr.mongodb.net/mini_projec
 });
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+app.use("/server/User",userRouter)
