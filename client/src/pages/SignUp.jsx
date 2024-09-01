b0c85746df27118d77cedb5792d6cb1f2a966f54
Ashyboban <ashyboban14@gmail.com>
2024-09-02 00:16:49 +0530

    complete signup page functional;ities

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,12 +1,74 @@
-import Nav from "./components/Nav"
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import {Link,useNavigate}from 'react-router-dom';
+import './SignUp.css';
 
 const SignUp = () => {
+  const [formData,setFormData]=useState({});
+  const [error,setError]=useState(null);
+  const [loading,setLoading]=useState(false);
+  const navigate=useNavigate();
+  const handleChange=(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id]:e.target.value,
+    });
+  };
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    setLoading(true);
+    const res=await fetch('/server/auth/signup',
+      {
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'},
+        
+        body:JSON.stringify(formData),
+      }
+    );
+    const data =await res.json();
+    if(data.success==false){
+      setLoading(false);
+
+      setError(data.message);
+      return;
+    }
+    setLoading(false);
+    setError(null);
+    console.log(data);
+    navigate('/sign-in');
+  }
+  console.log(formData);
   return (
     <>
       <Nav />
-      <div className="main-body">SignUp</div>
+      <div className="main-body">
+        <div className="signup-container">
+          <h2>Sign Up</h2>
+          <form action="/signup" method="POST" onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="username">Name</label>
+              <input type="text" id="username" name="username" required onChange={handleChange}/>
+            </div>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" required onChange={handleChange}/>
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" required onChange={handleChange}/>
+            </div>
+            <div className="input-group">
+              <label htmlFor="confirm-password">Confirm Password</label>
+              <input type="password" id="confirm-password" name="confirm-password" required onChange={handleChange}/>
+            </div>
+            <button type="submit" className="submit-button" disabled={loading}>{loading?'loading...':'Sign Up'}</button>
+          </form>
+          <p style={{ fontSize: '10px' }}>Already have an account? <a href="/login">Login here</a></p>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -8,7 +8,7 @@ const hashedPassword=bcryptjs.hashSync(password,10);
 const newUser=new User({username,email,password:hashedPassword});
 try {
     await newUser.save()
-res.status(201).json('User created successfully!');
+res.status(200).json({message:'User created successfully!'});
 } catch (error) {
 next(error);
 }
