cb22db5d7ed25e353a322d6aa9f784bee70201ea
Ashyboban <ashyboban14@gmail.com>
2024-09-01 09:51:18 +0530

    half way database connect

--- client/src/pages/components/Nav.jsx
+++ client/src/pages/components/Nav.jsx
@@ -3,6 +3,12 @@ import { Link } from "react-router-dom"
 const Nav = () => {
   return (
     <nav>
+       <div className="navbar-site-name">
+        <h1>Online Stay Finder</h1>
+    
+
+      </div>
+
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
         <Link to="/about">About</Link>
--- server/index.js
+++ server/index.js
@@ -2,15 +2,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 mongoose.connect("mongodb+srv://ashy:ashy@cluster0.7xnsr.mongodb.net/mini_project?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
   console.log('connected to mongodb');
 }).catch((err)=>{
   console.log(err);
 });
 const app = express();
-
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 app.use("/server/User",userRouter)
+app.use('/server/auth',authRouter);
