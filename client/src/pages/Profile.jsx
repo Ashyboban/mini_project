529c857f75f21db2cb6c3514acd828c0579a0329
Ashyboban <ashyboban14@gmail.com>
2024-10-12 08:15:05 +0530

    create delete user listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -140,6 +140,22 @@ const Profile = () => {
       setShowListingError(true);
     }
   }
+  const handleListingDelete=async(listingId)=>{
+    try {
+      const res=await fetch(`/server/listing/delete/${listingId}`,{
+        method:'DELETE',
+
+      });
+      const data=await res.json();
+      if (data.success===false){
+        console.log(data.message);
+        return
+      }
+      setUserListings((prev)=>prev.filter((listing)=>listing._id!==listingId))
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <>
       <Nav />
@@ -237,7 +253,7 @@ const Profile = () => {
 
         </Link>
         <div className="flex flex-col items-center">
-        <button className="text-green-700 uppercase">Delete</button>
+        <button onClick={()=>handleListingDelete(listing._id)} className="text-green-700 uppercase">Delete</button>
         <button className="text-red-700 uppercase">Edit</button>
 
         </div>
